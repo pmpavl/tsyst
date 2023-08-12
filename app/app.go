f5295982f431ea53838c02fb47283588cc343875
Maxim Prokashev <prokashev.m.p@gmail.com>
2023-08-12 14:33:59 +0300

    Bug fix

--- app/app.go
+++ app/app.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/pkg/errors"
 	"github.com/pmpavl/tsyst/app/api"
@@ -13,8 +12,6 @@ import (
 	"github.com/pmpavl/tsyst/app/api/tests"
 	"github.com/pmpavl/tsyst/app/router"
 	"github.com/pmpavl/tsyst/db"
-	"github.com/pmpavl/tsyst/models"
-	"github.com/pmpavl/tsyst/pkg/constants"
 	"github.com/pmpavl/tsyst/resources"
 	"github.com/rs/zerolog"
 	"golang.org/x/sync/errgroup"
@@ -49,23 +46,6 @@ func (a *App) Start(ctx context.Context) error {
 			Handler: rtr.Handler(),
 		}
 	)
-
-	dbTests.Create(ctx, models.NewTest(
-		"multiplication_repeatable",
-		"Умножение (Повторяемый)",
-		"Тест по теме умножение (Повторяемый). В этом тесте собраны простые задачи с умножением одного числа на другое. В тесте 4 задачи. Одна из задач указана конкретно, остальные выбираются динамически на основании тегов. По результатам теста, можно собрать аналитику, и вставить в дипломную работу.",
-		models.NewTestTags(
-			constants.ComplexityNormal,
-			constants.ClassNumbers{constants.ClassSix},
-			5,
-			constants.Duration(time.Minute*60),
-		),
-		models.NewTestRepeat(
-			constants.Repeatable,
-			constants.Duration(time.Minute*10),
-		),
-		nil,
-	))
 
 	group, ctx := errgroup.WithContext(ctx)
 
