3b5ed9d42c24d681db6e169355f2a7515b8983eb
Maxim Prokashev <prokashev.m.p@gmail.com>
2023-08-24 22:17:24 +0300

    Accept all CORS

--- app/router/middlewares/cors.go
+++ app/router/middlewares/cors.go
@@ -11,7 +11,7 @@ const maxAge time.Duration = 12 * time.Hour
 
 func CORS() gin.HandlerFunc {
 	return gincors.New(gincors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:80", "https://localhost:443"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PATCH"},
 		AllowHeaders:     []string{"Content-Length", "Content-Type", "X-Access-Token", "X-Refresh-Token"},
 		AllowCredentials: true,
